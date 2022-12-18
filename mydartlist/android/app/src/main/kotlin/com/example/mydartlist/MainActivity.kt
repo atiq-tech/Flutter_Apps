438fd03fcc49af9e8e79d163a8cefc00a0907187
Atiqur Rahman Atiq <59407612+atiq-tech@users.noreply.github.com>
2022-12-18 20:28:57 +0600

    Dart_List

---
+++ mydartlist/android/app/src/main/kotlin/com/example/mydartlist/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mydartlist
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ mydartlist/lib/map.dart
@@ -0,0 +1,9 @@
+void main() {
+  List<int> numbers = [23, 45, 17, 45, 100, 23, 67, 45, 10];
+  print(numbers);
+  print("Applying Add(700)");
+  numbers.add(700);
+  print("Applying insert(3,30)");
+  numbers.insert(3, 30);
+  print(numbers);
+}
